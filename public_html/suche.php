72f1fd0f4e797345531ac313d31ee5faa6d45f1a
momomomo11 <muhammetacik@10.24.16.129>
2015-10-16 14:13:05 +0200

    mo

--- public_html/suche.php
+++ public_html/suche.php
@@ -6,12 +6,20 @@
     </head>
     <body>
         <?php
+        
+        $vname = $_POST['vname'];
+        $nname = $_POST['nname'];
+        $pw1 = $_POST['pw1'];
+        $email = $_POST['email'];
+        $user = $_POST['user'];
+        $geschl = $_POST['geschl'];
+        
        
 $connect = mysql_connect('localhost', 'root', '', 'parfum')
         or die('Fehler beim Aufbau der Verbindung');
     
-    $nname = $_POST['nname'];
-    $
+    
+    
 
     $conn = @mysql_connect($MYSQL_HOST, $MYSQL_USER, $MYSQL_PASS) 
             OR die("Error: ".mysql_error());
@@ -20,7 +28,7 @@ $connect = mysql_connect('localhost', 'root', '', 'parfum')
 
         
 $sql = "INSERT INTO user (Name, Nachname, username, Passwort, Email, Geschlecht)".
-                "VALUES('$vname', '$nname','$email','$user','$pw1')";
+                "VALUES('$vname', '$nname','$email','$user','$pw1','geschl')";
 
 
                          
